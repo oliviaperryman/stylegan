a2b853ded8967a88f8fd15a13a2c5e0f5353a075
Olivia Perryman <oliviap.soccer@gmail.com>
2020-01-29 17:06:47 -0500

    Change source of saved model for encoding images

--- encode_images.py
+++ encode_images.py
@@ -112,7 +112,11 @@ def main():
 
     # Initialize generator and perceptual model
     tflib.init_tf()
-    with dnnlib.util.open_url(args.model_url, cache_dir=config.cache_dir) as f:
+    # with dnnlib.util.open_url(args.model_url, cache_dir=config.cache_dir) as f:
+    #     generator_network, discriminator_network, Gs_network = pickle.load(f)
+    #model_path = "./results/vm/network-snapshot-002364.pkl"
+    model_path = "./results/other/network-snapshot-008040.pkl"
+    with open(model_path, "rb") as f:
         generator_network, discriminator_network, Gs_network = pickle.load(f)
 
     generator = Generator(Gs_network, args.batch_size, clipping_threshold=args.clipping_threshold, tiled_dlatent=args.tile_dlatents, model_res=args.model_res, randomize_noise=args.randomize_noise)
